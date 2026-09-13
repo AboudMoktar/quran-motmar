8b44c0b411ab7984cc567f707e9b7bfad0913f27
AboudMoktar <aboud.moktarhe@gmail.com>
2026-09-13 22:56:45 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export const ASSOCIATION_NAME = "الرابطة الوطنية للقرآن الكريم"
 export const BRANCH_CITY = "معتمر"
 export const BRANCH_LABEL = `الفرع المحلي ب${BRANCH_CITY}`
+export const MONTHLY_FEE = 20
+export const PAYMENT_ALERT_DAY = 15
