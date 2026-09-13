6e96546330bbef2d9c7d32eb9e0935c536ef21c3
AboudMoktar <aboud.moktarhe@gmail.com>
2026-09-13 21:27:28 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          500: "#c9a227",
+          600: "#a8841f",
+        },
+      },
+    },
+  },
   plugins: [],
 }
