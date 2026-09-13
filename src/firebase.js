19d801065825b3e389e7f6e7c612ecca06d781ca
AboudMoktar <aboud.moktarhe@gmail.com>
2026-09-13 11:24:59 +0100

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUou8SgBMCY-EE5_BES2PfsZo09Y-XEqo",
+  authDomain: "quran-motmar-7e99c.firebaseapp.com",
+  projectId: "quran-motmar-7e99c",
+  storageBucket: "quran-motmar-7e99c.firebasestorage.app",
+  messagingSenderId: "1089832085812",
+  appId: "1:1089832085812:web:78a3ca0949c8467aebdd61"
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
