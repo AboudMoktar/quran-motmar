d5d91e998bb08bc534c490f5438bc6c11e9ebea9
AboudMoktar <aboud.moktarhe@gmail.com>
2026-09-13 22:59:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Teachers from "./pages/Teachers"
 import Classes from "./pages/Classes"
 import Students from "./pages/Students"
 import Attendance from "./pages/Attendance"
+import Payments from "./pages/Payments"
 import Reports from "./pages/Reports"
 
 function AdminRoute({ children }) {
@@ -39,6 +40,7 @@ function AppContent() {
         <Route path="/students" element={<AdminRoute><Students /></AdminRoute>} />
         <Route path="/reports" element={<AdminRoute><Reports /></AdminRoute>} />
         <Route path="/attendance" element={<Attendance />} />
+        <Route path="/payments" element={<Payments />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Layout>
