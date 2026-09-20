6af3483ae09182cb69461787afb574714395c895
AboudMoktar <aboud.moktarhe@gmail.com>
2026-09-20 16:37:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { AuthProvider, useAuth } from "./context/AuthContext"
+import { ThemeProvider } from "./context/ThemeContext"
 import Login from "./pages/Login"
 import Layout from "./components/Layout"
 import Dashboard from "./pages/Dashboard"
@@ -63,7 +64,9 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <AppContent />
+        <ThemeProvider>
+          <AppContent />
+        </ThemeProvider>
       </AuthProvider>
     </BrowserRouter>
   )
